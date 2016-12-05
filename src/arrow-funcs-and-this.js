a9676662cfbee3d1b3c802527e36febf5ef2ce4a
Andrew Ilijic <ailijic@gmail.com>
2016-12-05 11:39:37 -0500

    Impliment CRUD methods.

--- src/arrow-funcs-and-this.js
+++ src/arrow-funcs-and-this.js
@@ -1,14 +1,47 @@
-console.log("Hello World");
+console.log('Hello World')
 
-console.log(this);
+// console.log(this);
 // console.log(global);
 
-var that = this;
+var that = this
 
-notStrict();
+notStrict()
 function notStrict () {
-  var name = 'Tim';
-  console.log("Not a strict function");
-  console.log(this);
-  console.warn(this === that);
+  var name = 'Tim'
+  var that2 = this
+  console.log('name: ', name)
+  console.log('Not a strict function')
+  console.log(this)
+  console.warn(this === that)
+  notstrictCall()
+  function notstrictCall () {
+    console.log('inside notstrictCall()')
+    // console.log(name)
+    console.log(that)
+    console.log(that2)
+  }
+
+  var arrowFunc = () => {
+    console.log(name)
+    console.log(this)
+    console.log(that)
+    console.log(that2)
+  }
+  arrowFunc()
+}
+
+strict()
+function strict () {
+  'use strict'
+  var name = 'Jon'
+  var that2 = this
+  console.log('name: ', name)
+  console.log(this)
+  strictCall()
+  function strictCall () {
+    console.log('inside strictCall()')
+    console.log(name)
+    console.log(that)
+    console.log(that2)
+  }
 }
--- src/snippets.js
+++ src/snippets.js
@@ -1,9 +1,69 @@
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/');
+var mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/')
 
 moongoose.connection.on('error', err => {
-  console.error('Could not connect. Error: ', err);
-});
+  console.error('Could not connect. Error: ', err)
+})
 
-mongoose.connection.once('open', )
+mongoose.connection.once('open', function () {
+  var snippetSchema = mongoose.Schema({
+    name: {type: String, unique: true},
+    content: String
+  })
+
+  var Snippet = mongoose.model('Snippet', snippetSchema)
+})
+
+var create = (name, content) => {
+  var snippet = {
+    name: name,
+    content: content
+  }
+  Snippet.create(snippet, function (err, snippet) {
+    if (err || !snippet) {
+      console.error('Could not create snippet', name)
+      mongoose.disconnect()
+      return
+    }
+    console.log('Created snippet', snippet.name)
+    mongoose.diconnect()
+  })
+}
+
+var read = name => {
+  Snippet.findOne({name: name}, (err, snippet) => {
+    if (err || !snippet) {
+      console.error('Could not read snippet', name)
+      mongoose.disconnect()
+      return
+    }
+    console.log('Read snippet', snippet.name)
+    console.log(snippet.content)
+    mongoose.disconnect()
+  })
+}
+
+var update = (name, content) => {
+  Snippet.findOneAndUpdate({name: name}, {content: content},
+  (err, snippet) => {
+    if (err || !snippet) {
+      console.error('Could not update snippet', name)
+      mongoose.disconnect()
+      return
+    }
+  })
+}
+
+var del = (name, content) => {
+  Snippet.findOneAndRemove({name: name}, (err, snippet) => {
+    if (err || !snippet) {
+      console.error('Could not delete snippet', name)
+      mongoose.disconnect()
+      return
+    }
+    console.log('Deleted snippet', snippet.name)
+    mongoose.disconnect()
+  })
+}
+
