9a20ad50f78db893cac43740445eb30f8a0127c6
Andrew Ilijic <ailijic@gmail.com>
2016-12-06 14:46:15 -0500

    Explictly  pass 'Snippet' to the rest of the prog.

--- src/snippets.js
+++ src/snippets.js
@@ -6,81 +6,82 @@ mongoose.connection.on('error', err => {
   console.error('Could not connect. Error: ', err)
 })
 
-var Snippet = undefined
-
 mongoose.connection.once('open', function () {
   var snippetSchema = mongoose.Schema({
     name: {type: String, unique: true},
     content: String
   })
 
-  Snippet = mongoose.model('Snippet', snippetSchema)
+  var Snippet = mongoose.model('Snippet', snippetSchema)
+  start(Snippet)
 })
 
-var create = (name, content) => {
-  var snippet = {
-    name: name,
-    content: content
+function start (Snippet) {
+  var create = (name, content) => {
+    var snippet = {
+      name: name,
+      content: content
+    }
+    Snippet.create(snippet, function (err, snippet) {
+      if (err || !snippet) {
+        console.error('Could not create snippet', name)
+        mongoose.disconnect()
+        return
+      }
+      console.log('Created snippet', snippet.name)
+      mongoose.disconnect()
+    })
   }
-  Snippet.create(snippet, function (err, snippet) {
-    if (err || !snippet) {
-      console.error('Could not create snippet', name)
-      mongoose.disconnect()
-      return
-    }
-    console.log('Created snippet', snippet.name)
-    mongoose.diconnect()
-  })
-}
 
-var read = (name) => {
-  Snippet.findOne({name: name}, (err, snippet) => {
-    if (err || !snippet) {
-      console.error('Could not read snippet', name)
+  var read = (name) => {
+    Snippet.findOne({name: name}, (err, snippet) => {
+      if (err || !snippet) {
+        console.error('Could not read snippet', name)
+        mongoose.disconnect()
+        return
+      }
+      console.log('Read snippet', snippet.name)
+      console.log(snippet.content)
       mongoose.disconnect()
-      return
-    }
-    console.log('Read snippet', snippet.name)
-    console.log(snippet.content)
-    mongoose.disconnect()
-  })
-}
+    })
+  }
 
-var update = (name, content) => {
-  Snippet.findOneAndUpdate({name: name}, {content: content},
-  (err, snippet) => {
-    if (err || !snippet) {
-      console.error('Could not update snippet', name)
+  var update = (name, content) => {
+    Snippet.findOneAndUpdate({name: name}, {content: content},
+    (err, snippet) => {
+      if (err || !snippet) {
+        console.error('Could not update snippet', name)
+        mongoose.disconnect()
+        return
+      }
+    })
+  }
+
+  var del = (name) => {
+    Snippet.findOneAndRemove({name: name}, (err, snippet) => {
+      if (err || !snippet) {
+        console.error('Could not delete snippet', name)
+        mongoose.disconnect()
+        return
+      }
+      console.log('Deleted snippet', snippet.name)
       mongoose.disconnect()
-      return
-    }
-  })
-}
+    })
+  }
 
-var del = (name) => {
-  Snippet.findOneAndRemove({name: name}, (err, snippet) => {
-    if (err || !snippet) {
-      console.error('Could not delete snippet', name)
+  main()
+  function main () {
+    if (process.argv[2] === 'create') {
+      create(process.argv[3], process.argv[4])
+    } else if (process.argv[2] === 'read') {
+      read(process.argv[3])
+    } else if (process.argv[2] === 'update') {
+      update(process.argv[3], process.argv[4])
+    } else if (process.argv[2] === 'delete') {
+      del(process.argv[3])
+    } else {
+      console.error('Command not recognized')
       mongoose.disconnect()
-      return
     }
-    console.log('Deleted snippet', snippet.name)
-    mongoose.disconnect()
-  })
-}
-
-main()
-function main () {
-  if (process.argv[2] == 'create') {
-    create(process.argv[3], process.argv[4])
-  } else if (process.argv[2] == 'read') {
-    read(process.argv[3])
-  } else if (process.argv[2] == 'update') {
-    update(process.argv[3], process.argv[4])
-  } else if (process.argv[2] == 'delete') {
-    del(process.argv[3])
-  } else {
-    console.error('Command not recognized')
-    mongoose.disconnect()
   }
 }
